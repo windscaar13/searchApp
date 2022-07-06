de9d571755fddcf567c9c76a973e0ac2e99cf072
Selva Kumar <windscaar13@gmail.com>
2022-07-06 14:21:06 +0530

    First Commit

---
+++ SQL_SCRIPT/script.sql
@@ -0,0 +1,6 @@
+CREATE TABLE code_lookup (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  code varchar(45) DEFAULT NULL,
+  description varchar(45) DEFAULT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
